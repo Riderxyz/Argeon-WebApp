b65691e9b5d4d0018fa9417baddbc5ea642104d4
DESKTOP-EUCDANG\ifavi <ifavilla@gmail.com>
2017-12-07 18:20:40 -0200

    Servi?os ampliados. ainda precisam ser melhorados. Grid correngando com bot?es mas ainda precisa parar de carregar infinitamente

--- src/app/Service/cache-service.service.ts
+++ src/app/Service/cache-service.service.ts
@@ -1,11 +1,20 @@
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
 import 'rxjs/add/operator/map';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 @Injectable()
 export class CacheServiceService {
-  UsuarioObj: any = { username: null }
-  DatabaseObj: any = { pagina: null }
-  TituloObj: any = { Grid:null,notification: null }
+  UsuarioObj: any = { username: null };
+  DatabaseObj: any = { Destino: null, Executar: null };
+  TituloObj: any = { Grid:null,notification: null };
+  private messageSource = new BehaviorSubject<string>("Ola pessoas")
+  MensagemAtual = this.messageSource.asObservable()
   constructor() { }
+
+
+MudarDados(message:string){
+  this.messageSource.next(message)
+}
+
 
 }
