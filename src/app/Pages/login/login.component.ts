0cf29810661362f0f1c3cb9935dd38974c232b5a
DESKTOP-EUCDANG\ifavi <ifavilla@gmail.com>
2018-01-14 22:59:47 -0200

    Login em 10%

--- src/app/Pages/login/login.component.ts
+++ src/app/Pages/login/login.component.ts
@@ -2,20 +2,34 @@ import { Component, OnInit } from '@angular/core';
 import { CacheServiceService } from './../../Service/CacheSrv/cache-service.service';
 import { LoginSrvService } from './../../Service/LoginSrv/login-srv.service';
 import { RouterModule, Router } from '@angular/router';
+import { AngularFireAuth } from 'angularfire2/auth';
+import * as firebase from 'firebase/app';
+import { Observable } from 'rxjs/Observable';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
-  constructor(public LoginSrv: LoginSrvService, public router: Router, public cacheSrv: CacheServiceService) { }
+  UsernameDisplay: any
+  user: Observable<firebase.User>;
+  constructor(public LoginSrv: LoginSrvService, public router: Router, public cacheSrv: CacheServiceService, public afAuth: AngularFireAuth) {
+    //this.UsernameDisplay = 'teste123'
+  }
 
   ngOnInit() {
   }
 
   login() {
     this.LoginSrv.Login()
+    this.afAuth.authState.subscribe(user => {
+        if (user) this.UsernameDisplay = user.displayName
+        console.log(user)
+      })
+
+      //this.SetNameuser(user.displayName)
+      console.log('Aqui esta o que eu preciso', this.UsernameDisplay)
+   
   }
 
   logout() {
